8d7a4cfab61da2f86e78500cb7c945567a6d5fed
kazumoto <motch0214@gmail.com>
2019-10-18 01:02:17 +0900

    Setup css module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,17 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import styles from "./index.module.scss"
+
+console.warn(styles.imageContainer)
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div className={styles.imageContainer}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
